84a38f21d598c4f800dc0e26bd52f0492a6791a6
Ankit Sharma <ankitsm08@gmail.com>
2026-07-21 17:08:19 +0530

    fix(nvim): presistence.nvim with Neotree and adjust keybinds

--- nvim/.config/nvim/lua/plugins/persistence.lua
+++ nvim/.config/nvim/lua/plugins/persistence.lua
@@ -3,22 +3,76 @@ return {
     "folke/persistence.nvim",
     event = "BufReadPre",
     opts = {
-      options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" },
+      options = {
+        "blank",
+        "buffers",
+        "curdir",
+        "folds",
+        "help",
+        "tabpages",
+        "winsize",
+        "winpos",
+        "terminal",
+        "localoptions",
+      },
     },
+    config = function(_, opts)
+      require("persistence").setup(opts)
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "PersistenceSavePre",
+        callback = function()
+          pcall(vim.cmd, "Neotree close")
+        end,
+      })
+    end,
     keys = {
       {
-        "<leader>qS",
-        function()
-          require("persistence").save()
-        end,
-        desc = "[Q]uit [S]ave session",
-      },
-      {
-        "<leader>qL",
+        "<leader>qc",
         function()
           require("persistence").load()
         end,
-        desc = "[Q]uit [L]oad session",
+        desc = "Load [C]WD Session",
+      },
+      {
+        "<leader>qs",
+        function()
+          require("persistence").select()
+        end,
+        desc = "[S]elect Session",
+      },
+      {
+        "<leader>ql",
+        function()
+          require("persistence").load({ last = true })
+        end,
+        desc = "Load [L]ast Session",
+      },
+      {
+        "<leader>qd",
+        function()
+          require("persistence").stop()
+        end,
+        desc = "Stop/[Q]uit Persistence",
+      },
+      {
+        "<leader>qq",
+        "<cmd>wqa<cr>",
+        desc = "[Q]uit All (write)",
+      },
+      {
+        "<leader>qa",
+        "<cmd>qa!<cr>",
+        desc = "[Q]uit [A]ll (discard)",
+      },
+      {
+        "<leader>qw",
+        "<cmd>wq<cr>",
+        desc = "[W]rite & [Q]uit",
+      },
+      {
+        "<leader>qQ",
+        "<cmd>q!<cr>",
+        desc = "Force [Q]uit buffer",
       },
     },
   },

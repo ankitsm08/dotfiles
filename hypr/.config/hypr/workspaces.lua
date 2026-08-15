6dc7693af5d4b432adc8bfb1463fcbeae7b4c477
Ankit Sharma <ankitsm08@gmail.com>
2026-08-15 21:10:21 +0530

    monitor: change workspace setup, update profiles

--- hypr/.config/hypr/workspaces.lua
+++ hypr/.config/hypr/workspaces.lua
@@ -20,14 +20,14 @@ hl.monitor({
 
 -- External monitor (preferred)
 hl.workspace_rule({ workspace = "1", monitor = "HDMI-A-1", layout = "master", default = true, persistent = true })
-hl.workspace_rule({ workspace = "2", monitor = "HDMI-A-1", layout = "dwindle", default = true, persistent = true })
-hl.workspace_rule({ workspace = "3", monitor = "HDMI-A-1", layout = "dwindle", default = true, persistent = true })
+hl.workspace_rule({ workspace = "2", monitor = "HDMI-A-1", layout = "dwindle", persistent = true })
+hl.workspace_rule({ workspace = "3", monitor = "HDMI-A-1", layout = "dwindle", persistent = true })
 hl.workspace_rule({ workspace = "4", monitor = "HDMI-A-1", layout = "dwindle", persistent = true })
-hl.workspace_rule({ workspace = "5", monitor = "HDMI-A-1", layout = "scrolling", persistent = true })
-hl.workspace_rule({ workspace = "6", monitor = "HDMI-A-1", layout = "dwindle", persistent = true })
+hl.workspace_rule({ workspace = "7", monitor = "HDMI-A-1", layout = "scrolling", persistent = true })
+hl.workspace_rule({ workspace = "8", monitor = "HDMI-A-1", layout = "scrolling", persistent = true })
+hl.workspace_rule({ workspace = "9", monitor = "HDMI-A-1", layout = "scrolling", persistent = true })
+hl.workspace_rule({ workspace = "10", monitor = "HDMI-A-1", layout = "master", persistent = true })
 
 -- Laptop panel
-hl.workspace_rule({ workspace = "7", monitor = "eDP-1", layout = "master", default = true, persistent = true })
-hl.workspace_rule({ workspace = "8", monitor = "eDP-1", layout = "scrolling", persistent = true })
-hl.workspace_rule({ workspace = "9", monitor = "eDP-1", layout = "scrolling", persistent = true })
-hl.workspace_rule({ workspace = "10", monitor = "eDP-1", layout = "master", persistent = true })
+hl.workspace_rule({ workspace = "5", monitor = "eDP-1", layout = "scrolling", persistent = true })
+hl.workspace_rule({ workspace = "6", monitor = "eDP-1", layout = "dwindle", persistent = true })
